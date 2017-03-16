e513d92261f0618a191318c2daf1ccc2bb32c37d
Philip HARRELL <alightedlamp@gmail.com>
2017-03-16 14:10:59 -0500

    refactor and fix tweet popup

--- app.js
+++ app.js
@@ -7,8 +7,43 @@ require.config({
 
 require(['lib/modules/template' ,'lib/modules/quotes'], function(template) {
     var getQuote = function() {
+        var currentQuote = {};
         var idx = Math.floor(Math.random() * quotes.length);
-        return quotes[idx];
+        var quote = quotes[idx];
+
+        currentQuote.name = quote.name;
+        currentQuote.text = quote.text;
+
+        var qs = currentQuote.text + " - " + currentQuote.name;
+
+        if ((currentQuote.text.length + currentQuote.name.length) > 140) {
+          var namelen = currentQuote.name.length;
+          var qslen = 134 - namelen;
+          qs = currentQuote.text.substring(0, qslen) + "... - " + name;
+        }
+
+        currentQuote.link = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(qs);
+        currentQuote.bg = "url(assets/img/" + quote.src + ")";
+
+        return currentQuote;
+    }
+
+    var PopupCenter = function(url, title, w, h) {
+        // Fixes dual-screen position                         Most browsers      Firefox
+        var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+        var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+        var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+        var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+        var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+        var top = ((height / 2) - (h / 2)) + dualScreenTop;
+        var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+        // Puts focus on the newWindow
+        if (window.focus) {
+            newWindow.focus();
+        }
     }
 
     $(document).ready(function() {
@@ -16,5 +51,8 @@ require(['lib/modules/template' ,'lib/modules/quotes'], function(template) {
     });
     $("#new-quote").click(function() {
         template.showQuote(getQuote());
+    });
+    $("#tweet-link").click(function() {
+        PopupCenter(this.getAttribute('data-href'), "Share", 600, 250);
     });
 });
