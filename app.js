f2664acb04c6c66b7157fa3626fd5ba94140c870
René Clausen Nielsen <r@rcn.dk>
2018-07-11 13:21:24 +0200

    Add recipe language

--- app.js
+++ app.js
@@ -5,34 +5,35 @@ require.config({
   }
 });
 
-require(['lib/modules/template' ,'lib/modules/quotes'], function(template) {
+require(['lib/modules/template' ,'lib/modules/recipes'], function(template) {
     var idx = 0;
     var lastQuote;
 
     var getQuote = function() {
-        var getNewIdx = function() { return Math.floor(Math.random() * quotes.length); }
+        var getNewIdx = function() { return Math.floor(Math.random() * recipes.length); }
 
         var idx = getNewIdx();
         if (idx === lastQuote) getQuote();
         lastQuote = idx;
 
-        var quote = quotes[idx];
+        var recipe = recipes[idx];
         var currentQuote = {};
 
-        currentQuote.name = quote.name;
-        currentQuote.url = quote.url;
-        currentQuote.text = quote.text;
+        currentQuote.name = recipe.name;
+        currentQuote.url = recipe.url;
+        currentQuote.text = recipe.text;
+        currentQuote.lang = recipe.lang;
 
         var qs = currentQuote.text + " - " + currentQuote.name;
 
-        if ((currentQuote.text.length + currentQuote.name.length) > 140) {
+        if ((currentQuote.text.length + currentQuote.name.length) > 280) {
           var namelen = currentQuote.name.length;
-          var qslen = 134 - namelen;
+          var qslen = 274 - namelen;
           qs = currentQuote.text.substring(0, qslen) + "... - " + currentQuote.name;
         }
 
         currentQuote.link = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(qs);
-        currentQuote.bg = "url(assets/img/" + quote.src + ")";
+        currentQuote.bg = "url(assets/img/" + recipe.src + ")";
 
         return currentQuote;
     }
@@ -58,7 +59,7 @@ require(['lib/modules/template' ,'lib/modules/quotes'], function(template) {
     $(document).ready(function() {
         template.showQuote(getQuote());
     });
-    $("#new-quote").click(function() {
+    $("#new-recipe").click(function() {
         template.showQuote(getQuote());
     });
     $("#tweet-link").click(function() {
